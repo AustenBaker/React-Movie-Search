0028ff7b500d1bd4093decd86bf75c800bad9f60
AustenBaker <austenbaker225@gmail.com>
2020-11-19 14:45:14 -0700

    Start seperating search results and movie details into seperate compenents.

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -6,34 +6,86 @@ class SearchMovies extends React.Component {
     super()
     this.state = {
       input: '',
-      input2: '',
-      movies: [],
-      details: '',
     }
   }
 
+  //live update search input in box
   updateInput = event => {
     this.setState({input: event.target.value})
   }
 
+  //use jQuery to search OMDB api then call renderMovies(response)
   searchMovie = () => {
     $.getJSON(`http://www.omdbapi.com/?apikey=a43b7ac2&s=${this.state.input}`)
       .then(this.renderMovies);
-      
   }
-  renderMovies = (response) => {
-    this.setState({movies: response.Search})
-    //console.log(response.Search)
+
+  //renders movies or error depending on search results
+  renderMovies = (response) => { 
+    var moviesHTML = ``;
+
+    if(response.Response.toUpperCase() === 'TRUE') {
+      //Render individual movie card results
+      for(let i=0; i<response.Search.length; i++){
+        moviesHTML += `
+          <div id="${response.Search[i].imdbID}" class="movieCard">
+            <img class="movieImg" src="${response.Search[i].Poster}" alt="${response.Search[i].Title}"></img>
+          </div>
+        `;
+      }
+
+      //update html before generating event listeners
+      $('#searchResultsContainer').html(moviesHTML);
+
+      //Render individual event listeners for each movie card
+      for(let i=0; i<response.Search.length; i++){
+        //When clicked calls getMovieDetails() passing that movie card's id
+        document.getElementById(response.Search[i].imdbID).addEventListener("click", () => {
+          this.getMovieDetails(response.Search[i].imdbID);
+        });
+      }
+
+    } else {
+
+      moviesHTML = `<h1>No results</h1>`;
+      $('#searchResultsContainer').html(moviesHTML);
+
+    }
   }
   
   getMovieDetails = (id) => {
-    
     $.getJSON(`http://www.omdbapi.com/?apikey=a43b7ac2&i=${id}`)
-      .then(this.renderDetails);
+      .then(this.renderMovieDetails);
   }
-  renderDetails = (response) => {
-    this.setState({details: response})
-    console.log(response);
+  renderMovieDetails = (response) => {
+    //Render movie detail page
+    var detailsHTML = `
+      <img id="detailsImg" src="`+response.Poster+`" alt="movie poster"></img>
+      <div id="detailsText">
+        IMBD:      <b>`+response.imdbRating+` / 10 </b> <br>
+        Metascore: <b>`+response.Metascore+` / 100 </b> <br>
+        Awards:    <b>`+response.Awards+` </b> <br><br>
+
+        Runtime:      `+response.Runtime+`utes <br>
+        Plot:         `+response.Plot+` <br><br>
+
+        Director(s):  `+response.Director+` <br>
+        Writer(s):    `+response.Writer+` <br>
+        Actors:       `+response.Actors+` <br>
+        Genre(s):     `+response.Genre+` <br><br>
+      <div>`;
+
+    //update html
+    $('#movieDetailsContainer').html(detailsHTML);
+
+    //Hide movie results and show details
+    document.getElementById("searchResultsContainer").style.display = "none"
+    document.getElementById("movieDetailsContainer").style.display = "flex"
+
+    document.getElementById("movieDetailsContainer").addEventListener("click", () => {
+      document.getElementById("searchResultsContainer").style.display = "flex"
+      document.getElementById("movieDetailsContainer").style.display = "none"
+    })
   }
 
   render(){
@@ -44,70 +96,22 @@ class SearchMovies extends React.Component {
           <input value={this.state.input}
             onChange={this.updateInput}
             className="searchInput"
-            placeholder="Fight Club"
+            placeholder="Back to the Future"
           />
           <button
             className="searchButton"
             onClick={() => {
               this.searchMovie()
-              document.getElementById("movieResultsContainer").style.display = "flex"
-              document.getElementById("detailsContainer").style.display = "none"
+              document.getElementById("searchResultsContainer").style.display = "flex"
+              document.getElementById("movieDetailsContainer").style.display = "none"
             }}
           >
             Search
           </button>
         </div>
 
-        <div 
-          className="detailsContainer" 
-          id="detailsContainer"
-          onClick= {() => { 
-            document.getElementById("movieResultsContainer").style.display = "flex"
-            document.getElementById("detailsContainer").style.display = "none"
-          }}
-        >
-          <img 
-            id="detailsImg" 
-            src={this.state.details.Poster} 
-            alt="movie poster"
-          >
-          </img>
-
-
-          <div id="detailsText">
-            IMBD: <b>{this.state.details.imdbRating} / 10</b> <br></br>
-            Metascore: <b>{this.state.details.Metascore} / 100</b> <br></br>
-            <b>Awards: {this.state.details.Awards}</b> <br></br> <br></br>
-
-            Runtime: {this.state.details.Runtime}utes <br></br>
-            Director(s): {this.state.details.Director} <br></br>
-            Actors: {this.state.details.Actors} <br></br>
-            Genre(s): {this.state.details.Genre} <br></br>
-            Writer(s): {this.state.details.Writer} <br></br><br></br>
-
-            Plot: {this.state.details.Plot} <br></br>
-          </div>
-        </div>
-
-
-        <div className="movieResultsContainer" id="movieResultsContainer">
-          {
-            this.state.movies.map( (movie) =>
-              <div 
-                key={movie.imdbID}
-                className="movieCard"
-                onClick= {() => {
-                    this.getMovieDetails(movie.imdbID)
-                    document.getElementById("movieResultsContainer").style.display = "none"
-                    document.getElementById("detailsContainer").style.display = "flex"
-                  }
-                }
-              >
-                <img className="movieImg" src={movie.Poster} alt="movie poster"></img>
-              </div>
-            )
-          }
-        </div>      
+        <div id="searchResultsContainer"></div>    
+        <div id="movieDetailsContainer"></div>
       </div>
     );
   }
