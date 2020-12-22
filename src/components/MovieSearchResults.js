cd33ff689b7ad663d180c5cfdd854095b33b96c7
AustenBaker <austenbaker225@gmail.com>
2020-12-21 21:45:48 -0700

    Card Flip added, with movie details on the back! :)

--- src/components/MovieSearchResults.js
+++ src/components/MovieSearchResults.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const MovieSearchResults = ({data, fetchDetails}) => {
+const MovieSearchResults = ({data, data2, fetchDetails}) => {
   //If data.Response != true, print loading
   if(!data.Response){
     return <h1 id="searchResultsContainer">Loading...</h1>
@@ -8,13 +8,35 @@ const MovieSearchResults = ({data, fetchDetails}) => {
     return(
       <div id="searchResultsContainer" className="bg-dark text-white">
         {data.Search.map( (movie, index) => 
-          <div 
-            key={index}
-            id={movie.imdbID} 
-            className="movieCard"
-            onClick={() => {fetchDetails(movie.imdbID)} }  
-          > 
-            <img className="movieImg" src={movie.Poster} alt={movie.Title}></img>
+          <div className="movieCardContainer">
+            <div 
+              key={index}
+              id={movie.imdbID} 
+              className="movieCardInner"
+              onMouseOver={ () => (fetchDetails(movie.imdbID)) }  
+            > 
+              <div className="movieCardFront">
+                <img className="movieImg" src={movie.Poster} alt={movie.Title}></img>
+              </div>
+             
+              <div className="movieCardBack">
+                <h3 className="text-center"> {data2.Title} </h3>
+                <h4>
+                  IMBD:      <b> {data2.imdbRating} / 10 </b> <br />
+                  Metascore: <b> {data2.Metascore} / 100 </b> <br /><br />
+                </h4>
+                <h6> {data2.Plot} </h6> <br />
+                  Genre(s):     {data2.Genre} 
+                <button
+                  className="fixed-bottom btn btn-info w-100"
+                  onClick={() => {
+                    fetchDetails(movie.imdbID)
+                    document.getElementById("movieDetailsContainer").style.display = "block";
+                    document.getElementById("searchResultsContainer").style.display = "none";
+                  }}
+                > Show All Details </button>
+              </div>
+            </div>
           </div>
         )}
       </div> 
