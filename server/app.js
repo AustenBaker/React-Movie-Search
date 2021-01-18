b6b6744b7bd5760dca709ac2596adb21a8658e97
AustenBaker <austenbaker225@gmail.com>
2021-01-18 13:58:01 -0700

    test

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "..", '/build')));
 app.use(express.static("public"));
 
-app.get('*', function (req, res) {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
