d5ef3b582e8de992530bd07571692a15bb1170cc
AustenBaker <austenbaker225@gmail.com>
2021-01-18 18:54:03 -0700

    finally?

--- server/app.js
+++ server/app.js
@@ -2,21 +2,18 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
+require('dotenv').config()
 var movieRouter = require('./routes/movies');
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "..", '/build')));
-
-app.use('/movies', movieRouter);
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './build/index.html'));
-});
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Listening on port " + PORT));
 
-module.exports = app;
+express()
+  .use(logger('dev'))
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use(express.static(path.resolve(__dirname, './build')))
+  .use('/movies', movieRouter)
+  .get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './build', 'index.html'));
+})
+    .listen(PORT, () => console.log("Listening on port " + PORT));
+
