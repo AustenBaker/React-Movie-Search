3fe6459907aaeadce51b3a5ebe8a328cbcc6cf83
AustenBaker <austenbaker225@gmail.com>
2021-01-18 17:40:03 -0700

    test

--- server/app.js
+++ server/app.js
@@ -9,8 +9,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "..", '/build')));
-app.use(express.static("public"));
-
 
 app.use('/', indexRouter);
 app.use('/movies', movieRouter);
